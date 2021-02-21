7dffbe763892a43a7032fa5c4af2d899273aee5f
Meenu-Balagopal <75074137+Meenu-Balagopal@users.noreply.github.com>
2021-02-21 23:45:06 +0530

    Add files via upload

---
+++ graphics/dgraphics/cuboid.py
@@ -0,0 +1,4 @@
+def cuboid_area(l,b,h):
+	print(2*((l*b)+(b*h)+(l*h)))
+def cuboid_perimeter(l,b,h):
+	print(4*(l+b+h))
---
+++ graphics/rectangle.py
@@ -0,0 +1,4 @@
+def rectangle_area(l,b):
+	print(l*b)
+def rectangle_perimeter(l,b):
+	print(2*(l+b))
